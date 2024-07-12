7b546c05786c77b9d1ffb0478e3164cc193f5e5d
David Ikenna <sankkakeii@gmail.com>
2024-07-12 13:59:10 +0100

    change base url to live url

--- pages/api/backed/create-card.js
+++ pages/api/backed/create-card.js
@@ -1,51 +1,3 @@
-// import { BusinessCard } from '@/models';
-// import { v4 as uuidv4 } from 'uuid';
-
-// const apiRoute = async (req, res) => {
-//     const data = req.body;
-//     const logo = data.logo || null;
-//     const profile_picture = data.profilePicture || null;
-
-//     const card_id = uuidv4();
-//     const id = `${card_id}`;
-//     const ext_card_url = `http://localhost:3000/cards/${id}`;
-
-//     try {
-//         const new_card = await BusinessCard.create({
-//             id, // Ensure id is included
-//             first_name: data.firstName,
-//             last_name: data.lastName,
-//             job_title: data.jobTitle,
-//             email: data.email,
-//             phone: data.phone,
-//             logo,
-//             profile_picture,
-//             social_media: JSON.parse(data.social_media || '[]'), // Ensure this is a valid JSON string
-//             products_services: JSON.parse(data.products_services || '[]'), // Ensure this is a valid JSON string
-//             card_url: ext_card_url, // Ensure card_url is included
-//         });
-
-//         res.status(200).json({ success: true, message: 'Digital Business Card created successfully', card_url: ext_card_url });
-//     } catch (error) {
-//         console.log('ERROR:::::::::::', error);
-//         res.status(500).json({ success: false, message: 'Internal server error' });
-//     }
-// };
-
-// export default apiRoute;
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { BusinessCard } from '@/models';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -56,7 +8,8 @@ const apiRoute = async (req, res) => {
 
     const card_id = uuidv4();
     const id = `${card_id}`;
-    const ext_card_url = `http://localhost:3000/cards/${id}`;
+    // const ext_card_url = `http://localhost:3000/cards/${id}`;
+    const ext_card_url = `https://digicard-eta.vercel.app/cards/${id}`;
 
     try {
         const new_card = await BusinessCard.create({
@@ -73,7 +26,7 @@ const apiRoute = async (req, res) => {
             card_url: ext_card_url, // Ensure card_url is included
         });
 
-        res.status(200).json({ success: true, message: 'Digital Business Card created successfully', card_url: ext_card_url, card_id: new_card.id });
+        res.status(200).json({ success: true, message: 'Digital Business Card created successfully', card_url: ext_card_url });
     } catch (error) {
         console.log('ERROR:::::::::::', error);
         res.status(500).json({ success: false, message: 'Internal server error' });
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import React from 'react'
 export default function Home() {
   return (
     <section>
-      <Link className="text-blue-500 underline ml-8 mt-3 hover:text-blue-700" href="http://localhost:3000/cards/58303464-371f-4ff2-89eb-da43d42e010e">Demo Card Link</Link>
+      {/* <Link className="text-blue-500 underline ml-8 mt-3 hover:text-blue-700" href="http://localhost:3000/cards/58303464-371f-4ff2-89eb-da43d42e010e">Demo Card Link</Link> */}
+      <Link className="text-blue-500 underline ml-8 mt-3 hover:text-blue-700" href="https://digicard-eta.vercel.app/cards/58303464-371f-4ff2-89eb-da43d42e010e">Demo Card Link</Link>
       <DigiCard />
     </section>
   )
